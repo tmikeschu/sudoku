feffda06076b47902a1cdb9a760b2b1e903257cc
Mike Schutte <tmikeschutte@gmail.com>
2024-10-19 17:40:32 -0400

    prevent dup reds

--- src/game/Dot.tsx
+++ src/game/Dot.tsx
@@ -11,11 +11,11 @@ export const Dot = ({
       className={cn(
         "inline-flex rounded-full w-6 h-6",
         {
-          "bg-red-500": value === 1,
-          "bg-orange-500": value === 2,
-          "bg-yellow-500": value === 3,
-          "bg-green-500": value === 4,
-          "bg-emerald-700": value === 5,
+          "bg-orange-500": value === 1,
+          "bg-yellow-500": value === 2,
+          "bg-green-500": value === 3,
+          "bg-emerald-700": value === 4,
+          "bg-cyan-300": value === 5,
           "bg-blue-500": value === 6,
           "bg-violet-600": value === 7,
           "bg-indigo-800": value === 8,
